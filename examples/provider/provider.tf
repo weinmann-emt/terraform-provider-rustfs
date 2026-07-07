35d270a396f327961aaecde5adb7d94f0983d1d0
dennismdejong <dennis@caberdo.nl>
2026-07-07 11:42:32 +0200

    fix: Go version to real release, enable gosec, fix example, remove empty test (#39)

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,8 @@
-provider "scaffolding" {
-  # example configuration here
+provider "rustfs" {
+  # endpoint can also be set via RUSTFS_ENDPOINT env var
+  endpoint = "127.0.0.1:9001"
+  # access_key can also be set via RUSTFS_USER env var
+  access_key = "admin"
+  # access_secret can also be set via RUSTFS_SECRET env var
+  access_secret = var.rustfs_secret
 }
