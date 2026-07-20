cd9eb5b70eeb42498736a2b91844fc86aa89e0af
dennismdejong <dennis@caberdo.nl>
2026-07-20 11:20:52 +0200

    fix: remove duplicate testAccCheckUserDestroy from name test file (#72)

--- provider/rustfs_user_name_resource_test.go
+++ provider/rustfs_user_name_resource_test.go
@@ -2,13 +2,10 @@ package provider
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/terraform"
-	"github.com/weinmann-emt/terraform-provider-rustfs/pkg/rustfs"
 )
 
 func TestAccUserResource_nameDefault(t *testing.T) {
@@ -93,30 +90,4 @@ resource "rustfs_user" "test" {
   %s
 }
 `, accessKey, nameAttr)
-}
-
-func testAccCheckUserDestroy(s *terraform.State) error {
-	client := testAccRustClient()
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "rustfs_user" {
-			continue
-		}
-		accessKey := rs.Primary.Attributes["access_key"]
-		if accessKey == "" {
-			continue
-		}
-		_, err := client.ReadUserAccount(accessKey)
-		if err == nil {
-			return fmt.Errorf("user %s still exists", accessKey)
-		}
-	}
-	return nil
-}
-
-func testAccRustClient() rustfs.RustfsAdmin {
-	return rustfs.New(&rustfs.RustfsAdminConfig{
-		Endpoint:     os.Getenv("RUSTFS_ENDPOINT"),
-		AccessKey:    os.Getenv("RUSTFS_USER"),
-		AccessSecret: os.Getenv("RUSTFS_SECRET"),
-	})
 }
