e6d73104bab4b86040c77888a9f9a0c75a3f4f82
dennismdejong <55197148+dennismdejong@users.noreply.github.com>
2026-07-22 20:41:49 +0200

    feat: add bucket metadata backup data source and import resource (#62) (#77)
    * feat: add bucket metadata backup data source and import resource (#62)
    
    * test: add unit tests for bucket metadata export/import API client (#77)
    
    ---------
    
    Co-authored-by: weinmann-kay <127417931+weinmann-kay@users.noreply.github.com>

--- provider/provider.go
+++ provider/provider.go
@@ -126,6 +126,7 @@ func (p *RustfsProvider) Resources(ctx context.Context) []func() resource.Resour
 		NewServiceAccountRessource,
 		NewBucketRessource,
 		NewquotaRessource,
+		NewBucketMetadataBackupImportResource,
 		NewGroupResource,
 		NewBucketLifecycleConfigurationRessource,
 		NewTierResource,
@@ -140,6 +141,7 @@ func (p *RustfsProvider) Resources(ctx context.Context) []func() resource.Resour
 func (p *RustfsProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
 		NewPoolsDataSource,
+		NewBucketMetadataBackupDataSource,
 		NewUsersDataSource,
 	}
 }
